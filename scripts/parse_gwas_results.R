e34432c1ec945cee6ae474fc9b4b640252f3e586
filippob <filippo.biscarini@gmail.com>
2022-03-01 08:33:47 +0100

    parsing GWAS results for target temperature and humidity

--- scripts/parse_gwas_results.R
+++ scripts/parse_gwas_results.R
@@ -81,3 +81,61 @@ dd <-filtered_results %>%
   group_by(Chrom, model) %>%
   summarise(pvalue, `-log10` = -log(pvalue), posterior_odds) %>%
   fwrite("results/table_significant_ssr.csv", sep = ",")
+
+fwrite(x = filtered_results, file = "results/significant_ssr_extended.csv", sep=",")
+
+
+####################################
+### target temperature and humidity
+####################################
+
+significance_threshold = 1e-3
+
+## Bayesian odds
+n_qtl = 25
+n_snp = 16039
+prior = n_qtl/n_snp # if 10 true associations are expected before the GWAS
+power = 0.7 # (based on 500 samples, 0.03 heritability)
+p_value = significance_threshold ## threshold
+
+posterior_odds = prior*power/p_value ## P(true_assoc)/(1-P(true_assoc))
+
+### get significant results ###
+### read pvalues
+# target_temp
+results <- fread("results/filtered_genotypes.csv_targetTemp_GWAS_glm.results")
+filtered_results <- filter(results, pvalue < significance_threshold) %>% rename(Chrom = Chr, Position = Pos)
+filtered_results$posterior_odds = (prior*power)/filtered_results$pvalue
+filtered_results$model = "temperature"
+
+# target_mois
+results <- fread("results/filtered_genotypes.csv_targetMois_GWAS_lm.results")
+temp <- filter(results, pvalue < significance_threshold) %>% rename(Chrom = Chr, Position = Pos)
+# temp <- select(temp, -effect) %>% rename(Chrom = Chr, Position = Pos)
+temp$posterior_odds = (prior*power)/temp$pvalue
+temp$model = "humidity"
+
+filtered_results <- bind_rows(filtered_results,temp)
+
+temperature <- filter(filtered_results, model == "temperature") %>% pull(SNP)
+humidity <- filter(filtered_results, model == "humidity") %>% pull(SNP)
+
+snps <- list("temperature" = temperature, "humidity" = humidity)
+
+temp <- group_by(filtered_results, Chrom) %>% summarise(N = n())
+filtered_results$N <- temp$N[match(filtered_results$Chrom,temp$Chrom)]
+
+p <- ggplot(filter(filtered_results, N>1), aes( x = Position, y = -log(pvalue))) + geom_jitter(aes(color = model), width = 0.5, size=2)
+p <- p + facet_wrap(~Chrom)
+p <- p + theme(axis.text.x = element_text(angle = 90))
+# p
+
+ggsave(filename = "results/significant_snps_temp_humidity.png", plot = p, device = "png", width = 10, height = 7)
+
+dd <-filtered_results %>%
+  group_by(Chrom, model) %>%
+  summarise(pvalue, `-log10` = -log(pvalue), posterior_odds) %>%
+  fwrite("results/table_significant_ssr_temp_mois.csv", sep = ",")
+
+fwrite(x = filtered_results, file = "results/significant_ssr_extended_temp_mois.csv", sep=",")
+
