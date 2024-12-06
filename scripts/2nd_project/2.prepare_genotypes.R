e2730e6f5dd077b70770db7fe1a4e7dfa4ec125e
filippob <filippo.biscarini@gmail.com>
2024-12-06 16:56:52 +0100

    added: parameters to take a subset of markers (from previous GWAS results, significant markers)

--- scripts/2nd_project/2.prepare_genotypes.R
+++ scripts/2nd_project/2.prepare_genotypes.R
@@ -12,6 +12,7 @@ fname = "Festuci-sequence-Alpy.xlsx"
 phenotype_file = "phenotypes.csv"
 sheet_num = 1
 exclude_target_locality = NULL
+subset_snps = "results/filtered_genotypes.csv_Age_GWAS_multinomial.results" ## file with SNPs to consider for the analysis (subset of the total n. of snps/alleles/markers)
 # exclude_target_locality = c("Lavisdalen","Ulvehaugen") ## vector or NULL
 keep_shifted_only = TRUE
 excl_sample = FALSE
@@ -32,6 +33,10 @@ phenotypes <- fread(file.path(prjfolder, phenotype_file))
 vec <- colnames(M) %in% phenotypes$sample
 M <- M[,vec]
 print(paste(ncol(M), "samples left after matching genotypes and phenotypes"))
+
+## recode from n. of reads mapped to presence/absence
+## probably using the n. of reads would introduce a bias: amplification, depth of sequencing, chance of mapping reads to one location etc.
+M_recoded = ifelse(M>0,1,0)
 
 ## get marker position on contig
 writeLines(" - getting the contig of SSR")
@@ -58,11 +63,28 @@ unfiltered_genotypes$position = unfiltered_genotypes |>
 M <- unfiltered_genotypes |>
   select(Allelle, contig,position) |>
   rename(allele = Allelle) |>
-  bind_cols(M)
+  bind_cols(M_recoded)
+
+## take subset of markers
+if (subset_snps != "" & !is.null(subset_snps)) {
+  
+  fname = file.path(prjfolder, subset_snps)
+  subset = fread(fname)
+  subset <- subset |>
+    filter(pvalue <= 0.05) |>
+    select(SNP, Chr, Pos) |>
+    rename(allele = SNP, contig = Chr, position = Pos)
+  
+  M <- inner_join(M, subset, by = c("allele", "contig", "position"))
+  
+}
 
 ## data cleaning
 writeLines(" - cleaning data: removing alleles with zero count in all samples")
-vec <- rowSums(M[,-c(1:3)]) != 0
+temp <- M[,-c(1:3)]
+nsample = ncol(temp)
+freqs = rowSums(temp)/nsample
+vec <- (freqs < 0.95 & freqs > 0.05)
 M <- M[vec,]
 print(paste(nrow(M), "markers left after cleaning"))
 
